b0afe6fcab75a28f324bc4f97c904a16dd9ce9ff
emma <hassling.emma@gmail.com>
2019-04-27 22:17:52 +0200

    Added searchbar to page

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -7,9 +7,24 @@ export default class Index extends Component {
         return (
             <div className="content">
             <br/>
-            <h3>Välkommen!</h3>
+            <p className="welc">Välkommen!</p>
                <div className="header">
-                   <img src={header} alt="header" />
+                   <img src={header} alt="header" style={{width:800, borderRadius: 400/2 }}/>
+                   <form className="search">
+                     <div className="input-select">
+                     <select data-trigger="" name="choices-single-defaul">
+                     <option placeholder="">Kategori</option>
+                     <option>Frukost</option>
+                     <option>Lunch</option>
+                     <option>Middag</option>
+                     <option>Dessert</option>
+                     <option>Barnfavoriter</option>
+                     <option>Special</option>
+                   </select>
+                     </div>
+                     <input type="search" placeholder="  Sök recept"></input>
+                     <button class="btn-search" type="button" style={{borderRadius: 200/2}}><i class="fas fa-search"></i></button>
+                     </form>
                     <p>This is going to be the header</p>
                 </div>
                 <div className="searchbar">
