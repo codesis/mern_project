ce77fa4bc534d8100cf346ffcf70732b5e5bd210
emma <hassling.emma@gmail.com>
2019-05-12 21:00:34 +0200

    Adding sign out

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import jwt_decode from 'jwt-decode'
+import setAuthToken from './utils/setAuthToken'
+import { setCurrentUser, logoutUser } from './actions/authActions'
 
 import { Provider } from 'react-redux'
 import store from './store'
@@ -15,6 +18,27 @@ import Contact from './components/contact/contact'
 import Register from './components/auth/register'
 import Login from './components/auth/login'
 import Navbar from './components/layout/navbar'
+import PrivateRoute from './components/private-route/PrivateRoute'
+import Dashboard from './components/dashboard/Dashboard'
+
+// Check for token to keep the user signed in
+if (localStorage.jwtToken) {
+  const token = localStorage.jwtToken
+  setAuthToken(token)
+ // decode token and get user info
+  const decoded = jwt_decode(token)
+ // set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded))
+
+ // check for expired token
+  const currentTime = Date.now() / 1000 // to get ms
+  if (decoded.exp < currentTime) {
+    // Sign out user
+    store.dispatch(logoutUser())
+   // redirect to sign in
+   window.location.href = './loggain'
+  }
+}
 
 class App extends Component {
   render() {
@@ -31,7 +55,10 @@ class App extends Component {
        <Route path="/om" exact component={About} />
        <Route path="/kontakt" exact component={Contact} />
        <Route path="/registrera" exact component={Register} />
-       <Route path="/loggain" exact component={Login} /> 
+       <Route path="/loggain" exact component={Login} />
+       <Switch>
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+       </Switch>
       </div>
       </Router>
       </Provider>
