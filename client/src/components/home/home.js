c1becf40d80a7080d7bfc2ef2c778a10eb0e75ed
emma <hassling.emma@gmail.com>
2019-07-05 10:48:47 +0200

    Editing visuals

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,37 +1,10 @@
 import React, { Component } from 'react'
-// import axios from 'axios'
-// import { Link } from 'react-router-dom'
 import './home.css'
-// import example from './demoImg.jpg'
 
 /**
- * Index holding the index page, displaying
- * one recipe from each category. This feature is
- * not yet completed.
+ * Index holding the index page
  */
 export default class Index extends Component {
-
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       recipe_title: '',
-//       recipe_cat: ''
-//     }
-//   }
-
-//   componentDidMount() {
-//     axios.get('/recept')
-//     .then(res => {
-//         console.log(res)
-//         this.setState({ 
-//           recipe_title: res.data.recipe_title,
-//           recipe_cat: res.data.recipe_cat
-//          })
-//     })
-//     .catch((err) => {
-//         console.log(err)
-//     })
-// }
 
     render() {
         return (
@@ -40,31 +13,3 @@ export default class Index extends Component {
         )
     }
 }
-
-//             <br/>
-//             <p className="welc"></p>
-//                 <br/>
-//                 <div className="gallery-home">
-//                   <img src={example} alt="frukost" width="600" height="400"/>
-//                   <div className="r-titel">{this.state.recipe_title}</div>
-//                 </div>
-//                 <div className="gallery-home">
-//                   <img src={example} alt="lunch" width="600" height="400"/>
-//                   <div className="r-titel">{this.state.recipe_title}</div>
-//                 </div>
-//                 <div className="gallery-home">
-//                   <img src={example} alt="middag" width="600" height="400"/>
-//                   <div className="r-titel">{this.state.recipe_title}</div>
-//                 </div>                
-//                 <div className="gallery-home">
-//                   <img src={example} alt="dessert" width="600" height="400"/>
-//                   <div className="r-titel">{this.state.recipe_title}</div>
-//                 </div>
-//                 <div className="gallery-home">
-//                   <img src={example} alt="barnfavoriter" width="600" height="400"/>
-//                   <div className="r-titel">{this.state.recipe_title}</div>
-//                 </div>
-//                 <div className="gallery-home">
-//                  <img src={example} alt="special" width="200" height="400"/>
-//                  <div className="r-titel">{this.state.recipe_title}</div>
-//                 </div>
