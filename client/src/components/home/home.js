76a17aacdf9f2e508f07802828ea295f77372db8
emma <hassling.emma@gmail.com>
2019-07-05 10:47:43 +0200

    Editing visuals

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+// import axios from 'axios'
+// import { Link } from 'react-router-dom'
 import './home.css'
-import example from './demoImg.jpg'
+// import example from './demoImg.jpg'
 
 /**
  * Index holding the index page, displaying
@@ -8,37 +10,61 @@ import example from './demoImg.jpg'
  * not yet completed.
  */
 export default class Index extends Component {
+
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       recipe_title: '',
+//       recipe_cat: ''
+//     }
+//   }
+
+//   componentDidMount() {
+//     axios.get('/recept')
+//     .then(res => {
+//         console.log(res)
+//         this.setState({ 
+//           recipe_title: res.data.recipe_title,
+//           recipe_cat: res.data.recipe_cat
+//          })
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     })
+// }
+
     render() {
         return (
             <div className="content">
-            <br/>
-            <p className="welc"></p>
-                <br/>
-                <div className="gallery-home">
-                  <img src={example} alt="frukost" width="600" height="400"/>
-                  <div className="r-titel">Här står frukostens titel</div>
-                </div>
-                <div className="gallery-home">
-                  <img src={example} alt="lunch" width="600" height="400"/>
-                  <div className="r-titel">Här står lunchens titel</div>
-                </div>
-                <div className="gallery-home">
-                  <img src={example} alt="middag" width="600" height="400"/>
-                  <div className="r-titel">Här står middagens titel</div>
-                </div>                
-                <div className="gallery-home">
-                  <img src={example} alt="dessert" width="600" height="400"/>
-                  <div className="r-titel">Här står dessertens titel</div>
-                </div>
-                <div className="gallery-home">
-                  <img src={example} alt="barnfavoriter" width="600" height="400"/>
-                  <div className="r-titel">Här står barnfavons titel</div>
-                </div>
-                <div className="gallery-home">
-                 <img src={example} alt="special" width="200" height="400"/>
-                 <div className="r-titel">Här står specialens titel</div>
-                </div>
-            </div>
+             </div>
         )
     }
 }
+
+//             <br/>
+//             <p className="welc"></p>
+//                 <br/>
+//                 <div className="gallery-home">
+//                   <img src={example} alt="frukost" width="600" height="400"/>
+//                   <div className="r-titel">{this.state.recipe_title}</div>
+//                 </div>
+//                 <div className="gallery-home">
+//                   <img src={example} alt="lunch" width="600" height="400"/>
+//                   <div className="r-titel">{this.state.recipe_title}</div>
+//                 </div>
+//                 <div className="gallery-home">
+//                   <img src={example} alt="middag" width="600" height="400"/>
+//                   <div className="r-titel">{this.state.recipe_title}</div>
+//                 </div>                
+//                 <div className="gallery-home">
+//                   <img src={example} alt="dessert" width="600" height="400"/>
+//                   <div className="r-titel">{this.state.recipe_title}</div>
+//                 </div>
+//                 <div className="gallery-home">
+//                   <img src={example} alt="barnfavoriter" width="600" height="400"/>
+//                   <div className="r-titel">{this.state.recipe_title}</div>
+//                 </div>
+//                 <div className="gallery-home">
+//                  <img src={example} alt="special" width="200" height="400"/>
+//                  <div className="r-titel">{this.state.recipe_title}</div>
+//                 </div>
